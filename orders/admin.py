a91b0824405c6c89a393856f32856cf580cb1ee0
khaiphanvan <khaipv.11@grad.uit.edu.vn>
2018-08-21 08:38:24 +0700

    continue pdf

--- orders/admin.py
+++ orders/admin.py
@@ -5,6 +5,8 @@ import datetime
 from django.http import HttpResponse
 from django.utils.text import slugify
 from unidecode import unidecode
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 
 class OrderItemInline(admin.TabularInline):
@@ -37,6 +39,15 @@ def export_to_csv(modeladmin, request, queryset):
 
 export_to_csv.short_description = 'Export to CSV'
 
+
+def order_detail(obj):
+    return mark_safe('<a href="{}">View</a>'.format(reverse('orders:admin_order_detail', args=[obj.id])))
+
+
+def order_pdf(obj):
+    return mark_safe('<a href="{}">PDF</a>'.format(reverse('orders:admin_order_pdf', args=[obj.id])))
+    order_pdf.short_description = 'Invoice'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     # fieldsets = [
@@ -45,7 +56,7 @@ class OrderAdmin(admin.ModelAdmin):
     # ]
     list_display = ['id', 'first_name', 'last_name', 'email',
         'address', 'postal_code', 'city', 'paid',
-        'created', 'updated']
+        'created', 'updated',order_detail,order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
