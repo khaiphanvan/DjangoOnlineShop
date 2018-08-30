122276bc9966f857e76b497067cb2118a9af9066
khaiphanvan <khaipv.11@grad.uit.edu.vn>
2018-08-30 08:34:14 +0700

    create coupon and translate language

--- DjangoOnlineShop/settings.py
+++ DjangoOnlineShop/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
     'coupons.apps.CouponsConfig',
+    'rosetta',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -108,8 +110,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
+from django.utils.translation import gettext_lazy as _
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGES = (
+    ('en', _('English')),
+    ('vn', _('Vietnam')),
+)
+
+LANGUAGE_CODE = 'en'
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale/'),
+)
+
 
 TIME_ZONE = 'UTC'
 
@@ -147,3 +160,4 @@ Configuration.configure(
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
--- cart/forms.py
+++ cart/forms.py
@@ -1,13 +1,14 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
-                                    choices=PRODUCT_QUANTITY_CHOICES,
-                                    coerce=int,
-                                    label='Số lượng')
+        choices=PRODUCT_QUANTITY_CHOICES,
+        coerce=int,
+        label=_('Quantity'))
     update = forms.BooleanField(required=False,
-                                    initial=False,
-                                    widget=forms.HiddenInput)
+                                initial=False,
+                                widget=forms.HiddenInput)
